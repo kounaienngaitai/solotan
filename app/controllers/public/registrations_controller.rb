35b138347d89b4ef9955c442813982c2d0533636
EC2 Default User <ec2-user@ip-172-31-7-196.ap-northeast-1.compute.internal>
2024-03-14 14:31:21 +0000

    [Add] User login and signup functionalities #3

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,20 @@
-# frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
+
+  private
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :handle_name, :birth_date, :postal_code, :address, :telephone_number])
+  end
+
+  def after_sign_up_path_for(resource)
+    users_mypage_path
+  end
+
+end
+
+
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -40,10 +53,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
+
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -59,4 +71,4 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-end
+
