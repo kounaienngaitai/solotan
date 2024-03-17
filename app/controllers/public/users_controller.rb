f41f1a46b121c0d3e604e3cbc0bc038f592c25d3
EC2 Default User <ec2-user@ip-172-31-7-196.ap-northeast-1.compute.internal>
2024-03-17 12:52:21 +0000

    Add user profile edit functionality #14

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,17 +1,41 @@
 class Public::UsersController < ApplicationController
+
+
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to users_mypage_path, notice: "会員情報が更新されました。"
+    else
+      render :edit
+    end
   end
 
   def confirm
   end
 
   def withdrawal
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name,
+                                 :first_name,
+                                 :last_name_kana,
+                                 :first_name_kana,
+                                 :handle_name,
+                                 :email,
+                                 :postal_code,
+                                 :address,
+                                 :telephone_number)
   end
 
 end
